7b00c4e934591a4c01596bd9a95a94fa6f590c00
ConfidenceP <mukwindidzacp@gmail.com>
2018-08-25 17:21:05 +0200

    Update funcs6.c

--- funcs6.c
+++ funcs6.c
@@ -42,6 +42,16 @@ int		pf_octal(va_list ap)
 	return (ft_put_unbr_base(oct, base));
 }
 
+int		pf_octal_b(va_list ap)
+{
+	unsigned int	oct;
+	char			*base;
+
+	oct = va_arg(ap, unsigned int);
+	base = "01234567";
+	return (ft_put_unbr_base(oct, base));
+}
+
 int		pf_woctal(va_list ap)
 {
 	unsigned long long	oct;
@@ -50,6 +60,16 @@ int		pf_woctal(va_list ap)
 	oct = va_arg(ap, unsigned long long);
 	if (oct == 0)
 		return (ft_putchar('0'));
+	base = "01234567";
+	return (ft_put_unbr_base(oct, base));
+}
+
+int		pf_woctal_b(va_list ap)
+{
+	unsigned long long	oct;
+	char				*base;
+
+	oct = va_arg(ap, unsigned long long);
 	base = "01234567";
 	return (ft_put_unbr_base(oct, base));
 }
